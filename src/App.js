c07cf4b26e89629b578c1cfe9bb04f1828f407d1
Dan Applegate <applegatedt@gmail.com>
2017-11-23 18:57:34 -0500

    Working, but trying to fix fonts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class ResourcePool extends Component {
-  render() {
-    const classes = `ResourcePool ${this.props.type}`;
-    return (
-      <div className={classes}>
-        <p className="number">{this.props.amount}</p>
-      </div>
-    );
+class Resource extends Component {
+  constructor(props) {
+    super(props);
+    this.onMinus = this.onMinus.bind(this);
+    this.onPlus = this.onPlus.bind(this);
   }
-}
 
-class ResourceProduction extends Component {
+  onMinus() {
+    this.props.onAmountChange(-1);
+  }
+
+  onPlus() {
+    this.props.onAmountChange(1);
+  }
+
   render() {
-    const classes = `ResourceProduction ${this.props.type}`;
+    const classes = `Resource ${this.props.name}`;
     return (
       <div className={classes}>
-        <p className="number">{this.props.amount}</p>
+        <p className="resource-info">
+          <button onClick={this.onMinus} className="minus">-</button>
+          <span className="number">{this.props.amount}</span>
+          <button onClick={this.onPlus} className="plus">+</button>
+        </p>
       </div>
     );
   }
@@ -28,31 +35,44 @@ class ResourceBoard extends Component {
     super(props);
     this.changeAmount = this.changeAmount.bind(this);
     this.resourceNames = ['megacredits', 'steel', 'titanium', 'plants', 'energy', 'heat'];
+    let poolState = {};
+    this.resourceNames.forEach((name) => {
+      poolState[name + '-pool'] = 0;
+      poolState[name + '-production'] = 0;
+    });
+    this.state = poolState;
   }
 
-  changeAmount(poolName, change) {
-    this.setState((prevState) => {
-      return {
-        poolName: prevState[poolName] + change
-      };
-    });
+  changeAmount(poolName) {
+    let board = this;
+    return (change) => {
+      board.setState((prevState) => {
+        let update = {};
+        update[poolName] = prevState[poolName] + change;
+        return update;
+      });
+    };
   }
 
   render() {
     const resourcePools = this.resourceNames.map((name) => {
+      const poolName = name + "-pool";
       return (
-        <ResourcePool key={name + "-pool"} type={name} amount="0" />
+        <Resource key={poolName} name={poolName} amount={this.state[poolName]} onAmountChange={this.changeAmount(poolName)} />
       );
     });
     const resourceProductions = this.resourceNames.map((name) => {
+      const productionName = name + "-production";
       return (
-        <ResourceProduction key={name + "-production"} type={name} amount="0" />
+        <Resource key={productionName} name={productionName} amount={this.state[productionName]} onAmountChange={this.changeAmount(productionName)} />
       );
     });
     return (
       <div className="ResourceBoard">
-        {resourcePools}
-        {resourceProductions}
+        <div className="resource-wrapper">
+          {resourcePools}
+          {resourceProductions}
+        </div>
       </div>
     );
   }
